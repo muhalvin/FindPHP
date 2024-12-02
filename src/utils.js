deca81d644a633e3526a1ab357e7ac4317a408c9
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-12-02 10:42:39 +0700

    Fix vercel.json

--- src/utils.js
+++ src/utils.js
@@ -3,17 +3,17 @@ import fs from 'fs';
 import path from 'path';
 
 // Vercel
-// const __dirname = new URL('.', import.meta.url).pathname;
+const __dirname = new URL('.', import.meta.url).pathname;
 
-// export const readLinks = () => {
-//     const filePath = path.join(__dirname, '../public/json/data.json');
-//     const rawData = fs.readFileSync(filePath, 'utf-8');
-//     return JSON.parse(rawData);
-// };
-
-// Local
 export const readLinks = () => {
     const filePath = path.join(__dirname, '../public/json/data.json');
     const rawData = fs.readFileSync(filePath, 'utf-8');
     return JSON.parse(rawData);
 };
+
+// Local
+// export const readLinks = () => {
+//     const filePath = path.join(__dirname, '../public/json/data.json');
+//     const rawData = fs.readFileSync(filePath, 'utf-8');
+//     return JSON.parse(rawData);
+// };
