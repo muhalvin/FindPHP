0456ab799e932e3501d812f92bae058a70a9cb71
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-12-02 10:35:16 +0700

    Fix import in utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,16 @@
 import fs from 'fs';
 import path from 'path';
 
-const __dirname = new URL('.', import.meta.url).pathname;
+// Vercel
+// const __dirname = new URL('.', import.meta.url).pathname;
 
+// export const readLinks = () => {
+//     const filePath = path.join(__dirname, '../public/json/data.json');
+//     const rawData = fs.readFileSync(filePath, 'utf-8');
+//     return JSON.parse(rawData);
+// };
+
+// Local
 export const readLinks = () => {
     const filePath = path.join(__dirname, '../public/json/data.json');
     const rawData = fs.readFileSync(filePath, 'utf-8');
