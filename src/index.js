378b86dac63921c775ea37143652fe7e4a6d25dc
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-12-02 10:47:50 +0700

    Fix assets

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.static('public/assets'));
 
 // Search endpoint
 app.get('/search', (req, res) => {
