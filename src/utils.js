3c0b786771bb4465c7f25d44152df045cc8cffd2
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-12-02 10:30:01 +0700

    Fix import in utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@
 import fs from 'fs';
 import path from 'path';
 
+const __dirname = new URL('.', import.meta.url).pathname;
+
 export const readLinks = () => {
     const filePath = path.join(__dirname, '../public/json/data.json');
     const rawData = fs.readFileSync(filePath, 'utf-8');
